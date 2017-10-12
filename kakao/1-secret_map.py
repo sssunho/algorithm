e1f45c68afdfc9f657d69a2ab37832e51cf8d321
sssunho <sh293zz@gmail.com>
2017-10-12 11:25:12 +0900

    -

--- kakao/1-secret_map.py
+++ kakao/1-secret_map.py
@@ -1,4 +1,3 @@
-import traceback
 def solution(n, arr1, arr2):
     answer = []
 
@@ -24,24 +23,3 @@ def solution(n, arr1, arr2):
     return answer
 
 solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])
-
-
-
-
-def solution1(n, arr1, arr2):
-    answer = []
-
-    # n의 크기 검사: 1 <= n <= 16
-    if 1 <= n <= 16:
-        # arr 길이 검사: len(arr) == n
-        if (len(arr1) <= n) and (len(arr2) <= n):
-            # arr의 각 요소 x를 2진수로 바꾼다
-            arr11 = [ str(bin(x))[2:] for x in arr1]
-
-            # arr의 이진수 변화 후 길이 검사: X <= n
-            # arr1과 arr2의 숫자의 길이가 다르면? 2진수로 바꿀 때 무조건 n의 길이로 채워준다
-
-            # 바꾼 arr1과 arr2를 비교: 그 자릿수에 1이면 1로 대치한다
-            pass
-
-    return answer
