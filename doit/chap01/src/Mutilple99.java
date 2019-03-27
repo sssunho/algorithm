b1f93b19bad33c0c6560cc54c9d30a7931d60382
sssunho <sssunho.lee@gmail.com>
2019-03-27 19:44:28 +0900

    주석으로 문제설명 작성

--- doit/chap01/src/Mutilple99.java
+++ doit/chap01/src/Mutilple99.java
@@ -1,3 +1,9 @@
+/**
+ * 2019-03-26
+ * Doit! 자료구조와 함께 배우는 알고리즘 [자바편]
+ * 01장 기본 알고리즘, 연습문제 Q12
+ * 오른쪽과 같이 위쪽과 왼쪽에 곱하는 수가 있는 곱셈표를 출력하는 프로그램을 작성하세요.
+ */
 public class Mutilple99 {
     public static void main (String[] args) {
         System.out.println(" | 1  2  3  4  5  6  7  8  9");
